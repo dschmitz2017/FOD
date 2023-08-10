693689121c7fd15b68b08e936895ac42a43d72d3
Anna Wilson <anna.wilson@heanet.ie>
2023-08-10 14:40:55 +0000

    reload celery when django manage.py detects change

---
+++ flowspec/management/commands/celery_worker.py
@@ -0,0 +1,20 @@
+import shlex
+import subprocess
+import sys
+
+from django.core.management.base import BaseCommand
+from django.utils import autoreload
+
+
+def restart_celery():
+    celery_worker_cmd = "celery -A flowspy worker -B --concurrency=2 -l info -f logs/celery.log"
+    cmd = f'pkill -f "{celery_worker_cmd}"'
+
+    subprocess.call(shlex.split(cmd))
+    subprocess.call(shlex.split(f"/opt/FOD/pythonenv {celery_worker_cmd}"))
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print("Starting celery worker with autoreload...")
+        autoreload.run_with_reloader(restart_celery)
